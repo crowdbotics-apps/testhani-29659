beff1fd37fe1c23b956e92fdcbe58f438a8c2305
Crowdbotics <team@crowdbotics.com>
2021-08-09 09:35:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "testhani_29659Identifier",
+  url: "https://testhani_29659Identifier.botics.co",
+  api: "https://testhani_29659Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
